844a814140f35f297525d68717f0116b3a00e3be
BGflor25 <94405900+BGflor25@users.noreply.github.com>
2023-07-14 22:52:48 +0200

    20230714-2252

--- src/reservation.js
+++ src/reservation.js
@@ -32,14 +32,36 @@ let invitationNumber = urlParams.get('code')
 console.log(invitationNumber)
 
 //DB gegevens opvragen (query) op basis van URL parameter
-
 const q = query(colRef, where("invitationNumber", "==", parseInt(invitationNumber)))
-    onSnapshot(q, (snapshot) => {
-        let guests = []
-        snapshot.docs.forEach((doc) => {
-            guests.push({ ...doc.data(), id: doc.id })
-        })
-        console.log(guests)
+let guests = []
+onSnapshot(q, (snapshot) => {
+    snapshot.docs.forEach((doc) => {
+        guests.push({ ...doc.data(), id: doc.id })
     })
+    console.log(guests)
+    let name1 = document.getElementById("name1")
+    let name2 = document.getElementById("name2")
+    if (guests.length >= 2) {
+        console.log("test")
+        name1.innerText = guests[0].surname
+        name2.innerText = guests[1].surname
+        document.getElementById("aantalPersonen1").innerText = "Jullie zijn"
+        document.getElementById("aantalPersonen2").innerText = "Zijn jullie "
+        document.getElementById("aantalPersonen3").innerText = "Zijn jullie "
+    }
+    else if (guests.length == 1) {
+        document.getElementById("moreThanOneGuest").remove()
+        document.getElementById("name2").remove()
+        name1.innerText = guests[0].surname
+        document.getElementById("aantalPersonen1").innerText = "Je bent "
+        document.getElementById("aantalPersonen2").innerText = "Ben je "
+    }
+    if (guests[0].inviteToDinner === false){
+        document.getElementById("avondfeest1").remove()
+        document.getElementById("avondfeest2").remove()
+    }
+})
+
+
 
 
